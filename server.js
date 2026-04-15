dfa09ab4bcc705c878aaee619c4f49da09d74707
nesmithproperties <texasinvestmentsteam@gmail.com>
2026-04-15 12:27:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const ejs = require('ejs');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Configure delimiters to handle Python/Jinja2 tags safely
+ejs.delimiter = '%';
+ejs.openDelimiter = '{';
+ejs.closeDelimiter = '}';
+
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'templates'));
@@ -13,8 +18,9 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 app.get('/', (req, res) => {
     res.render('index.html', (err, html) => {
         if (err) {
-            console.error('RENDER ERROR:', err);
-            return res.status(500).send('Check if base.html exists in templates folder');
+            console.error('Render error:', err);
+            // Fallback to send the file as plain text if EJS fails
+            return res.sendFile(path.join(__dirname, 'templates', 'index.html'));
         }
         res.send(html);
     });
