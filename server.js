3aad63ed0b4bc6a1e61ac901a8c026aee36c6157
nesmithproperties <texasinvestmentsteam@gmail.com>
2026-04-15 12:25:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-ejs.closeDelimiter = '}';
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('view engine', 'html');
 app.engine('html', ejs.renderFile);
-
-app.use('/static', express.static(path.join(__dirname, 'static')));
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'templates'));
 
+app.use('/static', express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index.html', (err, html) => {
+        if (err) {
+            console.error('RENDER ERROR:', err);
+            return res.status(500).send('Check if base.html exists in templates folder');
+        }
+        res.send(html);
+    });
 });
 
 app.listen(port, () => {
